5e5b3e880c38c9f4c06c632d858fc102d2c51795
Evgeniy Shcherbina <ixsci@yandex.ru>
2017-11-14 10:37:40 +0300

    Added evaluation order discrepancy example
    Added a telling example where the difference of evaluation order
    between built-in and overloaded operators in pre-17 C++ is shown.

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
-#include "Pods.h"
-#include "Person.h"
 
 using namespace std;
 
-void decomposeArrayCopyExample();
-void decomposeArrayRefExample();
-void decomposeStructExample();
-void decomposePersonExample();
-
-int main(int argc, char* argv[])
+class Int
 {
-    decomposeArrayCopyExample();
-    decomposeArrayRefExample();
-    decomposeStructExample();
-    decomposePersonExample();
-    return 0;
+public:
+    Int() = default;
+
+    Int(int val):
+            m_Value{val}
+    {
+    }
+
+    Int operator++(int)
+    {
+        Int tmp{*this};
+        ++m_Value;
+        return tmp;
+    }
+
+    bool operator&&(const Int& rhs) const
+    {
+        return m_Value && rhs.m_Value;
+    }
+private:
+    int m_Value = 0;
+};
+
+template <typename T>
+bool cleverFun(T& value)
+{
+    return (cout << "first\n", value++) &&
+           (cout << "second\n", value++);
 }
 
-void decomposeArrayCopyExample()
+int main()
 {
-    cout << "=== Copy Example ===\n";
-    int array[] = {1, 3, 5, 7, 9};
-    auto [a1, a2, a3, a4, a5] = array;
-    cout << "Third element before: " << a3 << "(" << array[2] << ")\n";
-    a3 = 4;
-    cout << "Third element after: " << a3 << "(" << array[2] << ")\n";
-}
-
-void decomposeArrayRefExample()
-{
-    cout << "=== Reference Example ===\n";
-    int array[] = {1, 3, 5, 7, 9};
-    auto& [a1, a2, a3, a4, a5] = array;
-    cout << "Third element before: " << a3 << "(" << array[2] << ")\n";
-    a3 = 4;
-    cout << "Third element after: " << a3 << "(" << array[2] << ")\n";
-}
-
-void decomposeStructExample()
-{
-    cout << "=== Structs Example ===\n";
-    auto [f, s, t] = SimpleStruct{};
-    cout << "first: " << f
-         << ", second: " << s
-         << ", third: " << t << "\n";
-    ComplexStruct complexStruct{};
-    auto& [simple, answer] = complexStruct;
-    complexStruct.simple.third = 3.33f;
-    cout << "simple.third: " << simple.third
-         << ", answer: " << answer << "\n";
-}
-
-void decomposePersonExample()
-{
-    cout << "=== Person Example ===\n";
-    Person einstain{"Albert", "Einstein", 997};
-    const auto [id, first, last] = einstain;
-    cout << "ID: " << id
-         << ", First name: " << first
-         << ", Last name: " << last << "\n";
-}
+    int i = -1;
+    Int complexI{-1};
+    cout << "==========SIMPLE INT============\n";
+    cleverFun(i);
+    cout << "==========COMPLEX INT============\n";
+    cleverFun(complexI);
+};

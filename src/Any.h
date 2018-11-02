7d19c180336938f77f217988d87700874e974ede
Evgeniy Shcherbina <ixsci@pm.me>
2018-11-02 14:22:56 +0300

    Rich man's Any implementation version 1

--- src/Any.h
+++ src/Any.h
@@ -3,42 +3,59 @@
 #include <typeindex>
 #include <stdexcept>
 #include <utility>
+#include <memory>
+#include <type_traits>
 
-struct Any
+namespace Details
 {
-private:
-    enum Action {Copy, Delete};
-    using MagicHandler_t = void*(*)(void*, Action);
-public:
-    template<typename T>
-    Any(const T& var) :
-        m_Data{new T{var}},
-        m_Type{typeid(var)},
-        m_MagicHandler{[](void* ptr, Action action)
+    class AnyBase
+    {
+    public:
+        virtual void* getData() = 0;
+        virtual std::unique_ptr<AnyBase> clone() const = 0;
+        virtual ~AnyBase() = default;
+    };
+
+    template <typename T>
+    class AnyBaseImpl final: public AnyBase
+    {
+    public:
+        AnyBaseImpl(const T& value):
+            m_Data{value}
+        {}
+
+        AnyBaseImpl(T&& value):
+            m_Data{std::move(value)}
+        {}
+
+        void* getData() override
         {
-            void* result = nullptr;
-            if(action == Copy)
-                result =  new T(*static_cast<T*>(ptr));
-            else
-                delete static_cast<T*>(ptr);
-            return result;
-        }}
+            return &m_Data;
+        }
+
+        std::unique_ptr<AnyBase> clone() const override
+        {
+            return std::make_unique<AnyBaseImpl<T>>(m_Data);
+        }
+    private:
+        T m_Data;
+    };
+}
+
+class Any final
+{
+public:
+    template<typename T, typename = std::enable_if_t<!std::is_same_v<std::decay_t<T>, Any>>>
+    Any(T&& var):
+        m_AnyImpl{std::make_unique<Details::AnyBaseImpl<std::decay_t<T>>>(std::forward<T>(var))}
     {}
 
     Any(const Any& rhs):
-        m_Data{rhs.m_MagicHandler(rhs.m_Data, Copy)},
-        m_Type{rhs.m_Type},
-        m_MagicHandler{rhs.m_MagicHandler}
+        m_AnyImpl{rhs.m_AnyImpl->clone()}
     {
     }
 
-    Any(Any&& rhs):
-        m_Data{rhs.m_Data},
-        m_Type{rhs.m_Type},
-        m_MagicHandler{rhs.m_MagicHandler}
-    {
-        rhs.m_MagicHandler = [](void*, Action) -> void* { return nullptr; };
-    }
+    Any(Any&& rhs) = default;
 
     Any& operator=(const Any& rhs)
     {
@@ -47,36 +64,23 @@ public:
         return *this;
     }
 
-    Any& operator=(Any&& rhs)
-    {
-        swap(*this, rhs);
-        return *this;
-    }
-
-    ~Any()
-    {
-        m_MagicHandler(m_Data, Delete);
-    }
+    Any& operator=(Any&& rhs) = default;
 
     template <typename T>
     friend T AnyCast(const Any& any);
     friend void swap(Any& lhs, Any& rhs)
     {
         using std::swap;
-        swap(lhs.m_Data, rhs.m_Data);
-        swap(lhs.m_Type, rhs.m_Type);
-        swap(lhs.m_MagicHandler, rhs.m_MagicHandler);
+        swap(lhs.m_AnyImpl, rhs.m_AnyImpl);
     }
 private:
-    void* m_Data;
-    std::type_index m_Type;
-    MagicHandler_t m_MagicHandler;
+    std::unique_ptr<Details::AnyBase> m_AnyImpl;
 };
 
 template <typename T>
 T AnyCast(const Any& any)
 {
-    if(any.m_Type == typeid(T))
-        return *(static_cast<T*>(any.m_Data));
+    if(dynamic_cast<Details::AnyBaseImpl<T>*>(any.m_AnyImpl.get()))
+        return *(static_cast<T*>(any.m_AnyImpl->getData()));
     throw std::logic_error("Any doesn't hold object of specified type");
 }

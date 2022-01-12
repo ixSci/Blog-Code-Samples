d5389350936078c19743618848e1d274089ba7da
Evgeniy Shcherbina <ixsci@pm.me>
2022-01-12 10:06:49 +0300

    Implemented Variant v2.1
    Implementation with anonymous union.

--- src/Variant.cpp
+++ src/Variant.cpp
@@ -5,15 +5,20 @@
 using std::construct_at;
 using std::destroy_at;
 
+Variant::Variant():
+	m_Type{None}
+{
+}
+
 Variant::Variant(int integral):
 	m_Type{Integral},
-    m_Container{integral}
+    m_Integral{integral}
 {
 }
 
 Variant::Variant(const std::string& string):
 	m_Type{String},
-	m_Container{string}
+	m_String{string}
 {
 }
 
@@ -25,7 +30,7 @@ Variant::Variant(const Variant& rhs)
 Variant& Variant::operator=(const Variant& rhs)
 {
     if(type() == String)
-        destroy_at(&m_Container.string);
+        destroy_at(&m_String);
     _copy(rhs);
     return *this;
 }
@@ -33,7 +38,7 @@ Variant& Variant::operator=(const Variant& rhs)
 Variant::~Variant()
 {
     if(type() == String)
-	    destroy_at(&m_Container.string);
+	    destroy_at(&m_String);
 }
 
 Variant::Type Variant::type() const
@@ -44,40 +49,20 @@ Variant::Type Variant::type() const
 int Variant::integral() const
 {
     assert(type() == Integral);
-    return m_Container.integral;
+    return m_Integral;
 }
 
 const std::string& Variant::string() const
 {
     assert(type() == String);
-    return m_Container.string;
+    return m_String;
 }
 
 void Variant::_copy(const Variant& rhs)
 {
     if(rhs.type() == String)
-        construct_at(&m_Container.string, rhs.m_Container.string);
+        construct_at(&m_String, rhs.m_String);
     else if(rhs.type() == Integral)
-        construct_at(&m_Container.integral, rhs.m_Container.integral);
+        construct_at(&m_Integral, rhs.m_Integral);
     m_Type = rhs.type();
 }
-
-Variant::Container::Container()
-{
-}
-
-Variant::Container::Container(int val):
-	integral{val}
-{
-}
-
-Variant::Container::Container(const std::string& val):
-	string{val}
-{
-}
-
-Variant::Container::~Container()
-{
-}
-
-
--- src/Variant.h
+++ src/Variant.h
@@ -6,7 +6,7 @@ struct Variant
 public:
     enum Type{None, Integral, String};
 public:
-    Variant() = default;
+    Variant();
     Variant(int integral);
     Variant(const std::string& string);
     Variant(const Variant& rhs);
@@ -16,18 +16,13 @@ public:
     int integral() const;
     const std::string& string() const;
 private:
-    union Container
+    union
     {
-        Container();
-        Container(int val);
-        Container(const std::string& val);
-        ~Container();
-	    int integral;
-        std::string string;
+	    int m_Integral;
+        std::string m_String;
     };
 private:
     void _copy(const Variant& rhs);
 private:
-    Type m_Type = None;
-    Container m_Container;
+    Type m_Type;
 };

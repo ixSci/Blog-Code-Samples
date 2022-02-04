95173eba36c13315b284fea2c69e78127b3cd730
Evgeniy Shcherbina <ixsci@pm.me>
2022-02-04 11:44:18 +0300

    Implemented Variant v4.0
    Unconstrained template version with a byte array as an underlying
    storage.

--- src/Variant.h
+++ src/Variant.h
@@ -1,109 +1,150 @@
 #pragma once
 
-#include <cassert>
-#include <memory>
+#include <algorithm>
+#include <array>
 
-template<typename FirstT, typename SecondT>
+template<typename... Types>
+class Variant;
+
+namespace Details
+{
+    template<typename T, typename... Ts>
+    consteval int getTypeIndex()
+    {
+        std::array answers{std::is_same_v<std::decay_t<T>, std::decay_t<Ts>>...};
+        auto answer = std::find(answers.begin(), answers.end(), true);
+        return answer != answers.end() ? std::distance(answers.begin(), answer) : -1;
+    }
+
+    template<unsigned currentIndex, typename T, typename... Tail, typename Variant, typename F>
+    auto match(unsigned typeIndex, const Variant& variant, F&& function)
+    {
+        if(typeIndex == currentIndex)
+            return std::invoke(function, variant.template get<currentIndex>());
+        if constexpr(sizeof...(Tail) > 0)
+            return match<currentIndex + 1, Tail...>(typeIndex, variant, std::forward<F>(function));
+    }
+
+    template<unsigned currentIndex, typename T, typename... Tail>
+    void copy(unsigned sourceTypeIndex, const void* source, void* destination)
+    {
+        if(sourceTypeIndex == currentIndex)
+            std::construct_at(static_cast<T*>(destination), *static_cast<const T*>(source));
+        else if constexpr(sizeof...(Tail) > 0)
+            copy<currentIndex + 1, Tail...>(sourceTypeIndex, source, destination);
+    }
+
+    template<unsigned currentIndex, typename T, typename... Tail>
+    void destroy(unsigned typeIndex, void* storage)
+    {
+        if(typeIndex == currentIndex)
+            std::destroy_at(static_cast<T*>(storage));
+        else if constexpr(sizeof...(Tail) > 0)
+            destroy<currentIndex + 1, Tail...>(typeIndex, storage);
+    }
+}
+
+template<typename... Ts>
 class Variant
 {
 public:
-    Variant();
-    Variant(const FirstT& value);
-    Variant(const SecondT& value);
+    Variant() = default;
+    template<typename T> requires (Details::getTypeIndex<T, Ts...>() != -1)
+    Variant(T&& value);
     Variant(const Variant& rhs);
     Variant& operator=(const Variant& rhs);
+    template<typename T> requires (Details::getTypeIndex<T, Ts...>() != -1)
+    const T& get() const;
+    template<size_t Index> requires (Index < sizeof...(Ts))
+    const auto& get() const;
+    template<typename T> requires (Details::getTypeIndex<T, Ts...>() != -1)
+    bool is() const;
+    template<typename F>
+    auto match(F&& function) const;
     ~Variant();
-    template<typename Type>
-    const Type& value() const;
-private:
-    union
-    {
-	    FirstT m_First;
-        SecondT m_Second;
-    };
-    enum Type{None, First, Second};
 private:
     void _copy(const Variant& rhs);
     void _destroy();
 private:
-    Type m_Type;
+    int m_TypeIndex = -1;
+    alignas(Ts...) std::byte m_Storage[std::max({sizeof(Ts)...})];
 };
 
-template<typename FirstT, typename SecondT>
-Variant<FirstT, SecondT>::Variant():
-	m_Type{None}
+template<typename... Ts>
+template<typename T>
+requires (Details::getTypeIndex<T, Ts...>() != -1)
+Variant<Ts...>::Variant(T&& value):
+    m_TypeIndex{Details::getTypeIndex<T, Ts...>()}
 {
+    std::construct_at(reinterpret_cast<T*>(m_Storage), std::forward<T>(value));
 }
 
-template<typename FirstT, typename SecondT>
-Variant<FirstT, SecondT>::Variant(const FirstT& value):
-	m_Type{First},
-	m_First{value}
-	
-{
-}
-
-template<typename FirstT, typename SecondT>
-Variant<FirstT, SecondT>::Variant(const SecondT& value):
-	m_Type{Second},
-	m_Second{value}
-	
-{
-}
-
-template<typename FirstT, typename SecondT>
-Variant<FirstT, SecondT>::Variant(const Variant& rhs)
+template<typename... Ts>
+Variant<Ts...>::Variant(const Variant& rhs)
 {
     _copy(rhs);
 }
 
-template<typename FirstT, typename SecondT>
-Variant<FirstT, SecondT>& Variant<FirstT, SecondT>::operator=(const Variant& rhs)
+template<typename... Ts>
+Variant<Ts...>& Variant<Ts...>::operator=(const Variant& rhs)
 {
     _destroy();
     _copy(rhs);
     return *this;
 }
 
-template<typename FirstT, typename SecondT>
-Variant<FirstT, SecondT>::~Variant()
+template<typename... Ts>
+template<typename T>
+	requires (Details::getTypeIndex<T, Ts...>() != -1)
+const T& Variant<Ts...>::get() const
+{
+    if(is<T>())
+        return *(reinterpret_cast<const T*>(m_Storage));
+    throw std::logic_error("Wrong type!");
+}
+
+template <typename ... Ts>
+template <size_t Index> requires (Index < sizeof...(Ts))
+const auto& Variant<Ts...>::get() const
+{
+    using Type_t = std::tuple_element_t<Index, std::tuple<Ts...>>;
+    return get<Type_t>();
+}
+
+template<typename... Ts>
+template<typename T>
+	requires (Details::getTypeIndex<T, Ts...>() != -1)
+bool Variant<Ts...>::is() const
+{
+    return Details::getTypeIndex<T, Ts...>() == m_TypeIndex;
+}
+
+template<typename... Ts>
+template<typename F>
+auto Variant<Ts...>::match(F&& function) const
+{
+    if(m_TypeIndex == -1)
+        return;
+    return Details::match<0, Ts...>(m_TypeIndex, *this, std::forward<F>(function));
+}
+
+template<typename... Ts>
+Variant<Ts...>::~Variant()
 {
     _destroy();
 }
 
-template <typename FirstT, typename SecondT>
-template <typename Type>
-const Type& Variant<FirstT, SecondT>::value() const
+template<typename... Ts>
+void Variant<Ts...>::_copy(const Variant& rhs)
 {
-    if constexpr(std::is_same_v<Type, FirstT>)
-    {
-	    if(m_Type == First)
-            return m_First;
-    }
-    else if constexpr(std::is_same_v<Type, SecondT>)
-    {
-	    if(m_Type == Second)
-            return m_Second;
-    }
-    assert(false);
-    throw std::logic_error{"oops?!"};
+    if(rhs.m_TypeIndex != -1)
+        Details::copy<0, Ts...>(rhs.m_TypeIndex, rhs.m_Storage, m_Storage);
+    m_TypeIndex = rhs.m_TypeIndex;
 }
 
-template<typename FirstT, typename SecondT>
-void Variant<FirstT, SecondT>::_copy(const Variant& rhs)
+template<typename... Ts>
+void Variant<Ts...>::_destroy()
 {
-    if(rhs.m_Type == First)
-        std::construct_at(&m_First, rhs.m_First);
-    else if(rhs.m_Type == Second)
-        std::construct_at(&m_Second, rhs.m_Second);
-    m_Type = rhs.m_Type;
-}
-
-template <typename FirstT, typename SecondT>
-void Variant<FirstT, SecondT>::_destroy()
-{
-	if(m_Type == First)
-		std::destroy_at(&m_First);
-	else if(m_Type == Second)
-		std::destroy_at(&m_Second);
+    if(m_TypeIndex != -1)
+        Details::destroy<0, Ts...>(m_TypeIndex, m_Storage);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Variant.h"
 
+using std::cout;
 using namespace std::string_literals;
+
+template<class... Ts>
+struct overloaded: Ts...
+{
+    using Ts::operator()...;
+};
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+using Variant_t = Variant<int, std::string, float, double, char>;
 
 int main()
 {
-    Variant<int, std::string> var{55};
-    std::cout << "Var value=" << var.value<int>() << "\n";
+    Variant_t var{55};
+    std::cout << "Var value=" << var.get<int>() << "\n";
     var = "hey"s;
-    std::cout << "Var value=" << var.value<std::string>() << "\n";
-    var = 42;
-    std::cout << "Var value=" << var.value<int>() << "\n";
-    var = {};;
+    std::cout << "Var value=" << var.get<std::string>() << "\n";
+    var = 42.;
+    std::cout << "Var value=" << var.get<double>() << "\n";
+    var = {};
+    std::vector<Variant_t> variants = {{11}, {'C'}, {"string me"s}};
+    for(const auto& var : variants )
+    {
+        var.match(overloaded {
+            [](auto arg) { cout << "generic: " << arg << '\n';},
+            [](int arg) { cout << "int: " << arg << '\n'; },
+            [](char arg) { cout << "char: " << arg << '\n'; },
+            [](const std::string& arg) { cout << "string: " << arg << '\n'; }
+        });
+    }
 }
